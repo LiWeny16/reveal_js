ee36615c8f997f45abe0401fe86b9285b5e8830e
LiWeny16 <a454888395@gmail.com>
2023-08-10 23:50:11 +0800

    update

--- 0-React-模板/vite.config.js
+++ 0-React-模板/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir:"build"
+  build: {
+    outDir: "build"
   },
+  base: "./",
   resolve: {
     // ↓路径别名，主要是这部分
     alias: {
-        "@Plugin": resolve(__dirname, "./plugin")
+      "@Plugin": resolve(__dirname, "./plugin")
     }
-}
+  }
 })
